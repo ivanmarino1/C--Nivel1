50e5cc6f6f59e3bf35746dc504895a7de581cc62
ivanmarino1 <ivanamarillamarino@gmail.com>
2024-01-07 23:50:03 -0300

    commit

--- primer-nivel/unidad-7/C#/ejercicio-4/Program.cs
+++ primer-nivel/unidad-7/C#/ejercicio-4/Program.cs
@@ -13,5 +13,54 @@ class Program
         // a) El número de artículo que más se vendió en total.
         // b) Los números de artículos que no registraron ventas.
         // c) Cuantas unidades se vendieron del número de artículo 10.
+
+        int numero_de_articulo;
+        int cantidad_vendida;
+
+        int [] total_cantidad_vendidas = new int [15];
+
+        for (int i = 0; i < 15; i++) {
+            total_cantidad_vendidas[i] = 0;
+        }
+
+        Console.WriteLine("Ingrese el numero de articulo: ");
+        numero_de_articulo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la cantidad vendida: ");
+        cantidad_vendida = int.Parse(Console.ReadLine());
+
+        while (numero_de_articulo != 0) {
+
+            total_cantidad_vendidas[numero_de_articulo - 1] += cantidad_vendida;
+
+            Console.WriteLine("Ingrese el numero de articulo: ");
+            numero_de_articulo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad vendida: ");
+            cantidad_vendida = int.Parse(Console.ReadLine());
+        }
+
+        //Punto A)
+        int maximo_articulo = total_cantidad_vendidas[0];
+        int posicion_maximo = 1;
+
+        for (int i = 0; i < 15; i++) {
+            if (total_cantidad_vendidas[i] > maximo_articulo) {
+                maximo_articulo = total_cantidad_vendidas[i];
+                posicion_maximo = i + 1;
+            }
+        }
+
+        Console.WriteLine("El producto mas vendido es el: " + posicion_maximo + " con la cantidad de: " + maximo_articulo);
+
+        //Punto B)
+        for (int i = 0; i < 15; i++) {
+            if (total_cantidad_vendidas[i] == 0) {
+                Console.WriteLine("El producto " + (i + 1) " no tuvo ventas");
+            }
+        }
+
+        //Punto C)
+        Console.WriteLine("La cantidad vendida del articulo es: " + total_cantidad_vendidas[9]);
     }
 }
--- primer-nivel/unidad-7/C++/ejercicio-4/main.cpp
+++ primer-nivel/unidad-7/C++/ejercicio-4/main.cpp
@@ -12,7 +12,55 @@
 
 int main() {
 
-    
+    int numero_de_articulo;
+    int cantidad_vendida;
+
+    int total_cantidad_vendidas[15]; 
+
+    for (int i = 0; i < 15; i++) {
+        total_cantidad_vendidas[i] = 0;
+    }
+
+    std::cout << "Ingrese el numero de articulo: ";
+    std::cin >> numero_de_articulo;
+
+    std::cout << "Ingrese la cantidad vendida: ";
+    std::cin >> cantidad_vendida;
+
+    while (numero_de_articulo != 0) {
+
+        total_cantidad_vendidas[numero_de_articulo - 1] += cantidad_vendida; 
+
+        std::cout << "Ingrese el numero de articulo: ";
+        std::cin >> numero_de_articulo;
+
+        std::cout << "Ingrese la cantidad vendida: ";
+        std::cin >> cantidad_vendida;
+
+    }
+
+    //Punto A)
+    int maximo_articulo = total_cantidad_vendidas[0];
+    int posicion_maximo = 1;
+
+    for (int i = 0; i < 15; i++) {
+        if (total_cantidad_vendidas[i] > maximo_articulo) {
+            maximo_articulo = total_cantidad_vendidas[i];
+            posicion_maximo = i + 1;
+        }
+    }
+
+    std::cout << "El producto mas vendido es el: " << posicion_maximo << " con la cantidad de: " << maximo_articulo << std::endl;
+
+    //Punto B)
+    for (int i = 0; i < 15; i++) {
+        if (total_cantidad_vendidas[i] == 0) {
+            std::cout << "El producto " << i + 1 << " no tuvo ventas" << std::endl; 
+        }
+    }
+
+    //Punto C)
+    std::cout << "La cantidad vendida del articulo es: " << total_cantidad_vendidas[9];
 
     return 0;
 }
