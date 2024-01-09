69ade9501c7ca7cde4d1bd9e0443432998e11850
ivanmarino1 <ivanamarillamarino@gmail.com>
2024-01-08 22:09:13 -0300

    commit

--- primer-nivel/unidad-8/C#/ejercicio-2/Program.cs
+++ primer-nivel/unidad-8/C#/ejercicio-2/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         // Hacer una función llamada “par” que reciba un número entero y devuelva 1 si es par o cero si no lo es. Hacer un programa para ingresar 20 números y mostrar por pantalla cuántos son pares.
+
+        int numero;
+        int contador = 0;
+
+        for (int i = 0; i < 20; i++) {
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+            if (par(numero)) {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("La cantidad de numeros pares es: " + contador);
+    }
+
+    static bool par (int numero) {
+        if (numero % 2 == 0) {
+            return true
+        } else {
+            return false;
+        }
     }
 }
