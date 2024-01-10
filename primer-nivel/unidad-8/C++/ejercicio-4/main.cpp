cb5782bdbf79e46c555e50d451f2c082c8e44b06
ivanmarino1 <ivanamarillamarino@gmail.com>
2024-01-09 21:51:23 -0300

    commit

--- primer-nivel/unidad-8/C++/ejercicio-4/main.cpp
+++ primer-nivel/unidad-8/C++/ejercicio-4/main.cpp
@@ -6,9 +6,28 @@
 // b. -1 si el número es negativo.
 // c. 0 si el número es cero.
 
-int main() {
+void positivoNegativoCero(int numero, int& estado);
 
-    
+int main() {
+    int numero;
+    int estado = 5;
+
+    std::cout << "Ingrese un numero: ";
+    std::cin >> numero;
+
+    positivoNegativoCero(numero, estado);
+
+    std::cout << "El estado del valor " << numero << " es: " << estado << std::endl;
 
     return 0;
+}
+
+void positivoNegativoCero(int numero, int& estado) {
+    if (numero > 0) {
+        estado = 1;
+    } else if (numero == 0) {
+        estado = 0;
+    } else {
+        estado = -1;
+    }
 }
